2cb1b0acc8d7a3e0093f4b26de03ea10ed1b8d4e
Mohammad Kiani <mohammadkiani@outlook.com>
2019-11-03 20:16:50 -0500

    Initial Commit

---
+++ SegueApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApplication
+//
+//  Created by Mohammad Kiani on 2019-11-03.
+//  Copyright © 2019 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
